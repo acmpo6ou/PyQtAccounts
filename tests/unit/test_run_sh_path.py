8e564b3ce2d864a06b31bc00bda20d1f00afd647
Acmpo6ou <acmpo6ou@gmail.com>
2020-03-26 14:30:12 +0200

    Create tests directory for tests. Write first test to check path in run.sh.

---
+++ tests/unit/test_run_sh_path.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import unittest
+
+class RunShTest(unittest.TestCase):
+    '''
+    We don't want to accidentally push run.sh with incorrect path.
+    '''
+    def setUp(self):
+        self.file = open('run.sh')
+
+    def tearDown(self):
+        self.file.close()
+
+    def test_run_sh_path(self):
+        self.assertIn('cd ./core', self.file.read())
