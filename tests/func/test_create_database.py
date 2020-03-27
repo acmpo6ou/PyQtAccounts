7228e5dbe754b6677653bfbe44062eff78c05392
Acmpo6ou <acmpo6ou@gmail.com>
2020-03-27 15:38:16 +0200

    Finish password validation test.

--- tests/func/test_create_database.py
+++ tests/func/test_create_database.py
@@ -78,3 +78,26 @@ class CreateDbTest(unittest.TestCase):
         # because first input is filled and the second is not
         error = window.dbs.forms['create'].passEqError
         self.assertTrue(error.vis)
+
+        # Tom then filles second password field with the same password
+        pass_repeat_input = window.dbs.forms['create'].passRepeatField.passInput
+        QTest.keyClicks(pass_repeat_input, 'password123')
+
+        # The error disappears
+        self.assertFalse(error.vis)
+
+        # He then erase both password fields
+        pass_input.setText('')
+        pass_repeat_input.setText('')
+
+        # Another error appears saying that he needs to fill password fields
+        pass_error = window.dbs.forms['create'].passFilledError
+        self.assertTrue(pass_error.vis)
+
+        # Tom filles them again
+        QTest.keyClicks(pass_input, 'password123')
+        QTest.keyClicks(pass_repeat_input, 'password123')
+
+        # End there is no errors
+        self.assertFalse(error.vis)
+        self.assertFalse(pass_error.vis)
