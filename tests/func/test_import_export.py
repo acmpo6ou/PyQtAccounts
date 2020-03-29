3e77cfda1e2212c26c5dd082555179677a53e3f4
Acmpo6ou <acmpo6ou@gmail.com>
2020-03-29 18:48:57 +0300

    Write test for success import.

---
+++ tests/func/test_import_export.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from PyQt5.QtTest import QTest
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import unittest.mock
+import pytest
+import sys
+
+sys.path.append('.')
+
+from .base import BaseTest
+from utils import *
+from PyQtAccounts import *
+
+
+class ImportExportTest(BaseTest):
+    def setUp(self):
+        super().setUp()
+
+    def test_import_success(self):
+        # Emily wants to import database so she goes to menu File -> Import database
+        file = self.window.menuBar().actions()[0]  # first is `File` submenu
+        import_db = file.menu().actions()[1]  # second action is `Import database...`
+
+        # File dialog appears and she chose her tar file
+        self.monkeypatch.setattr(QFileDialog, 'getOpenFileName', lambda *args, **kwargs: (
+            '../tests/func/src/import_database.tar',))
+
+        # Success message appears, Emily presses `Ok` button
+        self.monkeypatch.setattr(QMessageBox, 'information',
+                                 lambda *args, **kwargs: QMessageBox.Ok)
+        import_db.trigger()
+
+        # Database appears in the list and on the disk
+        self.checkDbInList('import_database')
+        self.assertIn('import_database', getDbList())
