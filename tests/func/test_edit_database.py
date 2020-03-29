a8fc069bb54a669d2f2559f799d797d6b4ffd7b3
Acmpo6ou <acmpo6ou@gmail.com>
2020-03-29 12:02:55 +0300

    Add test for save button of edit database form.

--- tests/func/test_edit_database.py
+++ tests/func/test_edit_database.py
@@ -5,6 +5,7 @@ from PyQt5.QtCore import *
 import unittest
 import pytest
 import sys
+import os
 
 sys.path.append('.')
 
@@ -25,6 +26,14 @@ class EditDbTest(BaseTest):
         self.name = self.form.nameInput
         self.pass_input = self.form.passField.passInput
         self.pass_repeat_input = self.form.passRepeatField.passInput
+        self.saveButton = self.form.createButton
+
+        self.help = self.dbs.tips['help']
+
+    def openDatabase(self, name='database', password='some_password'):
+        self.list.selected(Index(name))
+        self.open_pass_input.setText(password)
+        self.open_form.openButton.click()
 
     def test_edit_db_warning(self):
         # Toon wants to edit database so he chose one from the list and presses edit button
@@ -35,12 +44,10 @@ class EditDbTest(BaseTest):
         self.checkOnlyVisible(self.dbs.tips['edit-w'], self.dbs)
 
         # So Toon opens database
-        self.list.selected(Index('database'))
-        self.open_pass_input.setText('some_password')
-        self.open_form.openButton.click()
+        self.openDatabase()
 
         # There is no errors
-        self.checkOnlyVisible(self.dbs.tips['help'], self.dbs)
+        self.checkOnlyVisible(self.help, self.dbs)
 
         # and then he tries again
         self.dbs.panel.editButton.click()
@@ -50,9 +57,7 @@ class EditDbTest(BaseTest):
 
     def test_name_validation(self):
         # Lea wants to edit database, so she opens one
-        self.list.selected(Index('database'))
-        self.open_pass_input.setText('some_password')
-        self.open_form.openButton.click()
+        self.openDatabase()
 
         # Then she presses edit button
         self.dbs.panel.editButton.click()
@@ -73,4 +78,50 @@ class EditDbTest(BaseTest):
         self.assertFalse(self.form.nameFilledError.visibility)
 
         # Save button is still enabled
-        self.assertTrue(self.form.createButton.isEnabled())
+        self.assertTrue(self.saveButton.isEnabled())
+
+    def test_save_button(self):
+        # pre-saving database
+        salt = open('../src/database.bin', 'rb').read()
+        db = open('../src/database.db', 'rb').read()
+
+        # Tom wants to edit database
+        self.openDatabase()
+
+        # He changes the name and password
+        self.name.setText('another_database')
+        self.pass_input.setText('another_password')
+        self.pass_repeat_input.setText('another_password')
+
+        # And presses the save button
+        self.saveButton.click()
+
+        # Edit database form disappears
+        self.checkOnlyVisible(self.help, self.dbs)
+
+        # Database name changes in the list
+        self.checkDbInList('another_database')
+
+        # And there is no longer `database` in the list
+        self.checkDbNotInList('database')
+
+        # Tom tries then to open `another_database`
+        self.openDatabase('another_database', 'another_password')
+
+        # There is no errors
+        self.checkOnlyVisible(self.help, self.dbs)
+
+        # database renamed on the disk
+        self.assertIn('another_database', getDbList())
+
+        # Database window appears
+        win = self.window.windows[1]  # first is main window, second is database one
+        self.assertTrue(win.visibility)
+
+        # restoring database
+        open('../src/database.bin', 'wb').write(salt)
+        open('../src/database.db', 'wb').write(db)
+
+        # cleaning up
+        os.remove('../src/another_database.bin')
+        os.remove('../src/another_database.db')
