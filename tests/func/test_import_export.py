459cfa17ce59049295975a4e0cb67b666044a53d
Acmpo6ou <acmpo6ou@gmail.com>
2020-03-30 09:39:24 +0300

    Write test for export database warning.

--- tests/func/test_import_export.py
+++ tests/func/test_import_export.py
@@ -16,10 +16,22 @@ from PyQtAccounts import *
 
 class ImportExportTest(BaseTest):
     def setUp(self):
+        self.settings = QSettings('PyTools', 'PyQtAccounts')
+        self.old_is_main_db = self.settings.value('advanced/is_main_db', False, type=bool)
+        self.old_main_db = self.settings.value('advanced/main_db', '', type=str)
+        
+        # Any database should be selected
+        self.settings.setValue('advanced/is_main_db', False)
         super().setUp()
 
+        self.exportWarning = self.dbs.tips['export']
+
+    def tearDown(self):
+        self.settings.setValue('advanced/is_main_db', self.old_is_main_db)
+        self.settings.setValue('advanced/main_db', self.old_main_db)
+
     def test_import_success(self):
-        # Emily wants to import database so she goes to menu File -> Import database
+        # Emily wants to import database so she goes to menu File -> Import database...
         file = self.window.menuBar().actions()[0]  # first is `File` submenu
         import_db = file.menu().actions()[1]       # second action is `Import database...`
 
@@ -56,3 +68,12 @@ class ImportExportTest(BaseTest):
 
         # Another error appears with the same message
         import_db.trigger()
+
+    def test_export_warning(self):
+        # Bob wants to export his database, so he goes to menu File -> Export database...
+        file = self.window.menuBar().actions()[0]  # first is `File` submenu
+        export_db = file.menu().actions()[2]       # second action is `Export database...`
+        export_db.trigger()
+
+        # Warning message appears saying that he needs to chose database first
+        self.checkOnlyVisible(self.exportWarning)
