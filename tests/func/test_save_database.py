05a1383a3853366fc459a8657ca9512693f12ea6
Acmpo6ou <acmpo6ou@gmail.com>
2020-04-04 11:27:11 +0300

    Write test for database save message (Yes button).

--- tests/func/test_save_database.py
+++ tests/func/test_save_database.py
@@ -36,11 +36,25 @@ class DbSaveTest(AccsTest):
         self.saveButton = self.form.createButton
         self.editButton = self.accs.panel.editButton
         self.name = self.form.nameInput
+        self.email = self.form.emailInput
 
         self.save_db = open('src/import_database.db', 'rb').read()
 
     def tearDown(self):
         open('src/import_database.db', 'wb').write(self.save_db)
+
+    def openDatabase(self):
+        window = Window()
+        dbs = window.dbs
+        form = dbs.forms['open']
+        _list = dbs.list
+        pass_input = form.passField.passInput
+        _list.selected(Index('import_database'))
+        pass_input.setText('import_database')
+        form.openButton.click()
+        win = window.windows[1]
+        accs = win.accs
+        return win
 
     def test_save_after_edit(self):
         # Ross wants to edit his account, so he chose it in the list and presses edit button
@@ -63,15 +77,7 @@ class DbSaveTest(AccsTest):
         del self.window
 
         # Ross then opens database again to check is everything saved
-        window = Window()
-        dbs = window.dbs
-        form = dbs.forms['open']
-        _list = dbs.list
-        pass_input = form.passField.passInput
-        _list.selected(Index('import_database'))
-        pass_input.setText('import_database')
-        form.openButton.click()
-        win = window.windows[1]
+        win = self.openDatabase()
         accs = win.accs
 
         # He chose his account at the list
@@ -79,3 +85,36 @@ class DbSaveTest(AccsTest):
 
         # And he sees his name changed at the account show form
         self.assertEqual("Ім'я: Ross Geller", accs.forms['show'].name.text())
+
+    def test_save_message_Yes(self):
+        # Lea wants to edit her account, so she chose it in the list
+        self.list.selected(Index('firefox'))
+        self.editButton.click()
+
+        # She change her e-mail to `spam@python.org`
+        self.email.setText('spam@python.org')
+
+        # But then Lea changed her mind and closes database window
+        # Message appears informing her about unsaved changes
+        # Lea presses `Yes`
+        self.monkeypatch.setattr(QMessageBox, 'information', self.mess(
+            'Увага!',
+            'Ви певні що хочете вийти?\n'
+            'Усі незбережені зміни буде втрачено!\n'
+            'Натисніть Ctrl+S аби зберегти зміни.', button=QMessageBox.Yes))
+        self.win.close()
+
+        # Database window is closed now and Lea closes PyQtAccounts
+        self.assertNotIn(self.win, self.window.windows)
+        self.window.close()
+        del self.window
+
+        # She then opens database again to check that changes aren't saved
+        win = self.openDatabase()
+        accs = win.accs
+
+        # She chose her account in the list
+        accs.list.selected(Index('firefox'))
+
+        # And sees that her e-mail is such as it was
+        self.assertEqual("E-mail: firefox@gmail.com", accs.forms['show'].email.text())
