ec5513b12e0dff9e9c4bea8a7bfdd791a5296213
Acmpo6ou <acmpo6ou@gmail.com>
2020-04-09 11:40:38 +0300

    Write test for Title class.

---
+++ tests/unit/setup.py/test_title.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2020 Kolvakh B.
+# This file is part of PyQtAccounts.
+
+# PyQtAccounts is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# PyQtAccounts is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with PyQtAccounts.  If not, see <https://www.gnu.org/licenses/>.
+
+from PyQt5.QtTest import QTest
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import unittest
+import pytest
+import os
+
+from tests.base import UnitTest
+from setup import *
+
+
+class TitleTest(UnitTest):
+    def test_title(self):
+        title = Title('Title text!')
+        self.assertEqual('<h4>Title text!</h4>', title.text())
+        self.assertEqual(Qt.AlignHCenter, title.alignment())
