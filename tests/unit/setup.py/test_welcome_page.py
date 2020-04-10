3698a167571bb6d6e1993ad188c08eea5d60f18a
Acmpo6ou <acmpo6ou@gmail.com>
2020-04-10 10:35:11 +0300

    Write test for welcome page.

---
+++ tests/unit/setup.py/test_welcome_page.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2020 Kolvakh B.
+# This file is part of PyQtAccounts.
+
+# PyQtAccounts is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# PyQtAccounts is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with PyQtAccounts.  If not, see <https://www.gnu.org/licenses/>.
+
+from PyQt5.QtTest import QTest
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import unittest
+import pytest
+import os
+
+from tests.base import UnitTest
+from setup import *
+
+
+class WelcomePageTest(UnitTest):
+    def test_welcome_page(self):
+        page = WelcomePage()
+        # There is an icon at the welcome page
+        icon = page.pixmap(QWizard.WatermarkPixmap).toImage()
+        expected_icon = QImage('/usr/share/icons/Mint-X/mimetypes/96/application-pgp-keys.svg')
+        self.assertEqual(icon, expected_icon)
+
+        # And title that welcomes you
+        title = page.title.text()
+        expected_title = '<h4><pre>Вітаємо у майстрі встановлення\n PyQtAccounts!</pre></h4>'
+        self.assertEqual(title, expected_title)
+
+        # There is also text that describes installation wizard
+        text = page.text.text()
+        expected_text = '<pre><br>Ми допоможемо вам пройти всі кроки \nвстановлення.</pre>'
+        self.assertEqual(text, expected_text)
