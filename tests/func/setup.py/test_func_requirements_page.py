9ea9d7eb3cc8fc44f4ffab2ed3a7fdc0367883d1
Acmpo6ou <acmpo6ou@gmail.com>
2020-04-20 08:51:05 +0300

    Finish this later.

--- tests/func/setup.py/test_func_requirements_page.py
+++ tests/func/setup.py/test_func_requirements_page.py
@@ -15,6 +15,7 @@
 
 # You should have received a copy of the GNU General Public License
 # along with PyQtAccounts.  If not, see <https://www.gnu.org/licenses/>.
+import time
 
 from PyQt5.QtTest import QTest
 from PyQt5.QtWidgets import *
@@ -24,62 +25,38 @@ import pyfakefs.fake_filesystem_unittest as fs_unit
 from unittest.mock import Mock
 import pytest
 import os
+import setup
 
-from tests.base import SetupFuncTest, SetupMixin
+from tests.base import UnitTest, SetupMixin
 from setup import *
 
-INSTALL_ERRORS_TEXT = (
-    'Не вдалося встановити cryptography\n'
-    'Не вдалося встановити gitpython\n'
-)
 
-
-class RequirementsPageTest(SetupFuncTest, SetupMixin):
-    def tearDown(self):
-        super().tearDown()
-        self.wizard.hide()
-
-    
+class RequirementsPageTest(UnitTest, SetupMixin):
     def test_install_no_pip(self):
         # Toon wants to install PyQtAccounts
         # He hasn't installed pip and some of dependencies
         self.patchReqs(['gitpython', 'pyshortcuts'], ['pip3'])
 
-        # So far he proceed to requirements page
-        self.wizard = InstallationWizard()
-        self.wizard.show()
-        self.next = self.wizard.button(QWizard.NextButton)
-        self.next.click()
-
-        # next button is unavailable until Toon installs all dependencies
-        self.assertFalse(self.next.isEnabled())
-
         # and he presses install button
-        page = self.wizard.currentPage()
+        page = RequirementsPage()
         page.installButton.click()
 
         # The error appears saying that he need to install pip first
         self.assertTrue(page.errors.visibility)
         self.assertEqual(page.errors.toPlainText(), 'Встановіть пакет pip3!')
 
-    @pytest.mark.last
+    @pytest.mark.skip
     def test_install_button(self):
         # Tom wants to install PyQtAccounts
         # He hasn't installed some pip dependencies
         self.to_install = ['cryptography', 'gitpython']
         self.patchReqs(self.to_install)
 
-        # So he proceed to the requirements page
-        self.wizard = InstallationWizard()
-        self.next = self.wizard.button(QWizard.NextButton)
-        self.wizard.show()
-        self.next.click()
-
         # installation will be successful
         self.monkeypatch.setattr('os.system', self.mock_system(False))
 
         # He presses install button
-        page = self.wizard.currentPage()
+        page = RequirementsPage()
         page.installButton.click()
 
         # He has pip3 installed so errors are cleared and hidden
@@ -109,31 +86,39 @@ class RequirementsPageTest(SetupFuncTest, SetupMixin):
         # tips are hidden
         self.assertFalse(page.reqsTips.visibility)
 
-    @pytest.mark.last
-    def test_errors_during_installation(self):
-        # Tom wants to install PyQtAccounts
-        # He hasn't installed some pip dependencies
-        to_install = ['cryptography', 'gitpython']
-        self.patchReqs(to_install)
 
-        # So he proceed to the requirements page
-        self.wizard = InstallationWizard()
-        self.next = self.wizard.button(QWizard.NextButton)
-        self.wizard.show()
-        self.next.click()
+def mock_system(res):
+    def wrap(command):
+        time.sleep(0.1)
+        # req = command.replace('pip3 install ', '')
+        # self.to_install.remove(req)
+        # self.patchReqs(self.to_install)
+        return res
+    return wrap
 
-        # installation will be unsuccessful
-        self.monkeypatch.setattr('os.system', self.mock_system(True))
+@pytest.mark.skip
+def test_errors_during_installation(monkeypatch):
+    # Tom wants to install PyQtAccounts
+    # He hasn't installed some pip dependencies
 
-        # He presses install button
-        page = self.wizard.currentPage()
-        page.installButton.click()
+    def testing(req):
+        if req in ['cryptography', 'git']:
+            raise ImportError()
 
-        # Some errors appearing during installation.
-        QTest.qWait(300)
-        print(repr(page.errors.toPlainText()))
-        self.assertEqual(page.errors.toPlainText(), INSTALL_ERRORS_TEXT)
-        self.assertTrue(page.errors.visibility)
+    print(Reqs().to_install)
 
-        # Next button is still disabled
-        self.assertFalse(self.next.isEnabled())
+    # installation will be unsuccessful
+    monkeypatch.setattr('os.system', lambda c: True)
+
+    # He presses install button
+    page = RequirementsPage()
+    page.installButton.click()
+
+    # Some errors appearing during installation.
+    QTest.qWait(100)
+    INSTALL_ERRORS_TEXT = (
+        'Не вдалося встановити cryptography\n'
+        'Не вдалося встановити gitpython\n'
+    )
+    assert page.errors.toPlainText() == INSTALL_ERRORS_TEXT
+    assert page.errors.visibility
