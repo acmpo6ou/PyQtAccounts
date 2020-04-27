1f8f7407bafeaefd7f6a21a019f9a62c8e3c58b3
Acmpo6ou <acmpo6ou@gmail.com>
2020-04-27 14:59:45 +0300

    Fully document utils.py module.

--- core/utils.py
+++ core/utils.py
@@ -25,15 +25,27 @@ import core.const
 
 
 def getDbList():
+    """
+    This function returns list of databases in your `src` directory, path to which is defined by
+    SRC_DIR constant from core.const module.
+    Function finds databases by their .db files.
+    """
     return [os.path.basename(db).replace('.db', '') for db in
             glob.glob(f'{core.const.SRC_DIR}/*.db')]
 
 
 def getAkiList(db):
+    """
+    This function returns list of accounts in your `db` database, which you pass as argument.
+    """
     return [acc for acc in db]
 
 
 def getVersion():
+    """
+    This function returns current version of the program.
+    Actually it returns last tag of program repository sorted by date.
+    """
     repo = git.Repo('.')
     tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
     version = tags[-1]
@@ -41,12 +53,29 @@ def getVersion():
 
 
 def hide(*args):
+    """
+    This function hides all widgets in all instances that are passed to it as arguments.
+    """
     for arg in args:
         for widget in arg:
             arg[widget].hide()
 
 
 def validName(name):
+    """
+    This function returns validated name (i.e. without unallowed characters).
+    We need it because we store databases in files called by their name (plus `.db` extension)
+    and you cant name files with for example this `/@` characters.
+    :param name:
+    name of database you need to validate.
+    :return:
+    validated name.
+    Example of usage:
+    >>> validName('hello\/@ it is name')                # all unallowed characters are removed
+    'helloitisname'
+    >>> validName('hello_this.is(allowed-characters)')  # '.()-_' -- are allowed characters
+    'hello_this.is(allowed-characters)'
+    """
     valid = ascii_letters + digits + '.()-_'
     result = ''
     for c in name:
@@ -56,10 +85,24 @@ def validName(name):
 
 
 class Index:
-    '''Fake index for main database settings.'''
+    """
+    Fake index for main database settings.
+    This class emulates StandardItemModel index that is passed to selected() signal when you
+    click at the QListView list element. This index stores the name of element being clicked.
+
+    We need this class to implement main database setting: when on startup we automatically
+    select main database which specified as `main_db` value of QSettings.
+    """
 
     def __init__(self, name):
         self.name = name
 
     def data(self):
+        """
+        This method is called when you try to get name of element which index represents.
+        Example of usage:
+        >>> i = Index('main')
+        >>> i.data()
+        'main'
+        """
         return self.name
