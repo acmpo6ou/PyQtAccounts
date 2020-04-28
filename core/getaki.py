05641075fb9263f8422ec0a033b05a769930cc2a
Acmpo6ou <acmpo6ou@gmail.com>
2020-04-28 12:46:05 +0300

    Write documentation for getaki.py module.

--- core/getaki.py
+++ core/getaki.py
@@ -16,6 +16,10 @@
 # You should have received a copy of the GNU General Public License
 # along with PyQtAccounts.  If not, see <https://www.gnu.org/licenses/>.
 
+"""
+This module provides functions for creating, encrypting and decrypting databases.
+"""
+
 import os
 import base64
 import core.akidump as akidump
@@ -28,16 +32,37 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
 def generateSalt(saltfile):
+    """
+    This function generates salt for cryptographic encryption and writes it to file.
+    :param saltfile:
+    represents path where file with salt will be located.
+    """
     salt = os.urandom(16)
     with open(saltfile, 'wb') as file:
         file.write(salt)
 
 
 def openDatabase(dbname, password):
+    """
+    This function opens database by its name and password.
+    :param dbname:
+    represents name of database
+    :param password:
+    represents password of database, must be byte string
+    :return:
+    opened database, type: dict
+    """
+
+    # here we construct path to database and its salt file by concatenating SRC_DIR constant,
+    # database name and .db or .bin extension,
+    # SRC_DIR represents name of folder in which database files are stored.
     dbfile = f'{core.const.SRC_DIR}/' + dbname + '.db'
     saltfile = f'{core.const.SRC_DIR}/' + dbname + '.bin'
+
     with open(saltfile, 'rb') as file:
         salt = file.read()
+
+    # here we using fernet encryption function to decrypt and deserialize database.
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
@@ -58,10 +83,25 @@ def openDatabase(dbname, password):
 
 
 def encryptDatabase(dbname, db, password):
+    """
+    THis function encrypts database.
+    :param dbname:
+    name of the database
+    :param db:
+    database, type: dict
+    :param password:
+    password of the database, type: byte string
+    :return:
+    encrypted database string
+    """
+    # here we construct path to salt file of database by concatenating SRC_DIR constant,
+    # database name and .bin extension
     saltfile = f'{core.const.SRC_DIR}/' + dbname + '.bin'
 
     with open(saltfile, 'rb') as file:
         salt = file.read()
+
+    # here we using fernet encryption function to encrypt and serialize database.
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
@@ -77,7 +117,16 @@ def encryptDatabase(dbname, db, password):
 
 
 def newDatabase(dbname, password):
-    print(core.const.SRC_DIR)
+    """
+    This function creates empty database and saves it to file.
+    :param dbname:
+    name of the database
+    :param password:
+    password of the database, type: byte string
+    """
+    # here we construct path to database and its salt file by concatenating SRC_DIR constant,
+    # database name and .db or .bin extension,
+    # SRC_DIR represents name of folder in which database files are stored.
     saltfile = f'{core.const.SRC_DIR}/' + dbname + '.bin'
     dbfile = f'{core.const.SRC_DIR}/' + dbname + '.db'
 
@@ -90,6 +139,9 @@ def newDatabase(dbname, password):
 
 
 def isEqual(first, second):
+    """
+    This function compares two databases and returns True if they are equal.
+    """
     if set(first) != set(second):
         return False
 
