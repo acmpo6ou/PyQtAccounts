70d2718fea0d6d96fd523cbf6a7829f595455f3d
Acmpo6ou <acmpo6ou@gmail.com>
2020-05-01 17:49:51 +0300

    Finished documentation for updates.py module.

--- core/updates.py
+++ core/updates.py
@@ -68,27 +68,52 @@ def time_for_updates():
 
 
 def getChangeLog():
+    """
+    This function downloads changelog from our repository.
+    :return:
+    list of change strings
+    """
     return [change.decode().rstrip() for change in urlopen(
         'https://raw.githubusercontent.com/Acmpo6ou/PyQtAccounts/master/change.log')]
 
 
 class Updating(QObject):
+    """
+    This class represents process that gets changelog.
+    """
+    # this is a signal that instance emits when updating process ends.
     result = pyqtSignal(bool, list)
 
     def run(self):
+        """
+        This method does all updating work, such as gets changelog.
+        """
         import git
         repo = git.Repo('.')
         origin = repo.remote()
         origin.fetch()
 
+        # we fetch all changes and if there new ones than changes list wouldn't be empty
+        # also we get changelog which we will show at the updating dialog if there are updates
         changes = list(repo.iter_commits('master..origin/master'))
         changelog = getChangeLog()
 
+        # after everything is done we emit resulting changes and changelog
         self.result.emit(bool(changes), changelog)
 
 
 class UpdatesAvailable(QWidget):
+    """
+    This class is a window that PyQtAccounts shows when there are updates available.
+    """
     def __init__(self, parent, log):
+        """
+        This constructor creates dialog with changelog and buttons: `Apply` and `Later`
+        :param parent:
+        parent for updates available dialog
+        :param log:
+        changelog of the update
+        """
         super().__init__()
         self.setParent(parent)
         self.setWindowTitle('Доступно нове оновлення')
@@ -96,6 +121,7 @@ class UpdatesAvailable(QWidget):
         self.resize(1000, 500)
         self.show()
 
+        # Here are title of the dialog with icon created and aligned
         self.title = Title('<h3>Доступно нове оновлення</h3>')
         self.title.setMinimumWidth(800)
         self.icon = QLabel()
@@ -105,12 +131,14 @@ class UpdatesAvailable(QWidget):
         header.addWidget(self.icon)
         header.addWidget(self.title)
 
+        # tip about restart after update
         tip = "Доступно нове оновлення PyQtAccounts.\n" \
               "Після оновлення програма перезапуститься.\n" \
               "Переконайтеся що ви зберігли всі зміни до ваших баз данних перед оновленням.\n"
         self.text = QLabel(tip)
+
+        # here we parse changelog and set its text to changelog label
         changelog = '<h4>Що нового:</h4><ul>'
-        print(log)
         for change in log:
             changelog += '<li>{}</li>\n'.format(change)
         changelog += '</ul>'
@@ -134,22 +162,40 @@ class UpdatesAvailable(QWidget):
         self.setLayout(layout)
 
     def applyUpdate(self):
+        """
+        This method does all update work, such as pulling all commits to programs repository.
+        """
+        # Here we hide updates available
         self.hide()
+
+        # download all changes
         repo = git.Repo('.')
         origin = repo.remote()
         origin.pull()
+
+        # start new instance of the program in daemon
         t = threading.Thread(target=os.system, args=('./run.sh',), daemon=True)
         t.start()
+
+        # and close the old one
         self.parent().close()
 
 
 class ShowChangelog(QDialog):
+    """
+    This class is a window for changelog that user sees through menu: Updates -> View changelog
+    """
     def __init__(self, parent):
+        """
+        Constructor of the dialog.
+        """
         QDialog.__init__(self, parent)
         self.setWindowTitle("Changelog")
         self.resize(800, 500)
         self.show()
 
+        # here we get current version of PyQtAccounts, parse changelog and set all this as
+        # labels text
         version = getVersion()
         changelog = '<h4>PyQtAccounts {}:</h4><ul>'.format(version)
         for change in open('change.log'):
