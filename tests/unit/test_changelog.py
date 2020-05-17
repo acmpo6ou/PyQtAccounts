a2d90492fa59c5fab75fe5adad46bf61c3228cca
Acmpo6ou <acmpo6ou@gmail.com>
2020-05-17 16:56:56 +0300

    Write documentation for test_changelog.py unit test module.

--- tests/unit/test_changelog.py
+++ tests/unit/test_changelog.py
@@ -16,6 +16,10 @@
 # You should have received a copy of the GNU General Public License
 # along with PyQtAccounts.  If not, see <https://www.gnu.org/licenses/>.
 
+"""
+This test module contains all unit tests for ShowChangelog dialog of PyQtAccounts.
+"""
+
 from PyQt5.QtTest import QTest
 from PyQt5.QtCore import *
 import unittest
@@ -27,17 +31,28 @@ from core.utils import *
 from PyQtAccounts import *
 
 
-class Test(UnitTest):
-    def setUp(self):
-        super().setUp()
-
+class ChangelogTest(UnitTest):
+    """
+    This test class contains all unit tests for ShowChangelog dialog of PyQtAccounts.
+    """
     def test_changelog(self):
+        """
+        This test tests whether text changelog in ShowChangelog dialog is constructed correctly.
+        """
         def mock_open(*args):
+            """
+            This is a test double for `open` built in function, we use it to create fake changelog
+            file that ShowChangelog dialog uses.
+            """
             return ['Fixed issues.', 'Changelog tested now.', 'Other updates.']
+
+        # here we pre-saving and mocking `open` function, also we patch version to check does
+        # ShowChangelog dialog shows it correctly too.
         normal_open = __builtins__['open']
         __builtins__['open'] = mock_open
         self.patchVersion()
 
+        # then we create ShowChangelog instance and check its changelog label content.
         log = ShowChangelog(None)
         right_text = '<h4>PyQtAccounts v2.0.6:</h4><ul><li>Fixed issues.</li>\n' \
                      '<li>Changelog tested now.</li>\n' \
@@ -46,4 +61,5 @@ class Test(UnitTest):
         self.assertEqual(right_text, log.changelogLabel.text())
 
         # clean up
+        # here we restore pre-saved `open` function
         __builtins__['open'] = normal_open
