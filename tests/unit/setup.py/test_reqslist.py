9cc7d5848b42c0df62c31b24951839df6393bcfe
Acmpo6ou <acmpo6ou@gmail.com>
2020-05-30 11:07:40 +0300

    Write documentation for test_reqslist.py

--- tests/unit/setup.py/test_reqslist.py
+++ tests/unit/setup.py/test_reqslist.py
@@ -26,34 +26,57 @@ import os
 from tests.base import UnitTest
 from setup import *
 
+# here are path to icons that represent installed and not installed requirements respectively
 installed = '/usr/share/icons/Humanity/actions/48/gtk-yes.svg'
 not_installed = '/usr/share/icons/Humanity/actions/48/stock_not.svg'
 
 
 class ReqsListTest(UnitTest):
+    """
+    This class provides tests for ReqsList.
+    """
     def test_icons(self):
-        self.assertTrue(os.path.exists(installed))
-        self.assertTrue(os.path.exists(not_installed))
+        """
+        This test tests does icons for installed and not installed requirements exist.
+        """
+        self.assertTrue(os.path.exists(installed),
+                        'Icon for installed requirements does not exist!')
+        self.assertTrue(os.path.exists(not_installed),
+                        'Icon for not installed requirements does not exist!')
 
     def test_reqs(self):
+        """
+        This test tests ReqsList itself.
+        """
+        # here we create fake Reqs with fake lists of installed and not installed requirements
         reqs = Mock()
         reqs.cant_install = ('git', 'xclip')
         reqs.to_install = ('gitpython',)
         reqs.installed = ('python3-pip', 'cryptography')
 
+        # and here we pass our fake reqs to ReqsList constructor creating ReqsList instance that
+        # we will test
         reqslist = ReqsList(reqs)
-        self.assertEqual(reqslist.editTriggers(), QAbstractItemView.NoEditTriggers)
-        self.assertEqual(reqslist.model, QListView.model(reqslist))  # We override model method
 
+        # here we check that user can't edit items in ReqsList
+        self.assertEqual(reqslist.editTriggers(), QAbstractItemView.NoEditTriggers,
+                         'Items of ReqsList can be edited!')
+
+        # and here we check that model in our requirements list contains exactly that amount of
+        # items that we specified by passing fake reqs in constructor
         model = reqslist.model
         length = len(reqs.cant_install) + len(reqs.to_install) + len(reqs.installed)
-        self.assertEqual(length, model.rowCount())
+        self.assertEqual(length, model.rowCount(), 'Length of ReqsList model is incorrect!')
 
+        # and here by iterating through all items of lists model we check that they have
+        # appropriate icon
         for i in range(model.rowCount()):
             req = model.item(i).text()
             icon = model.item(i).icon().pixmap(QSize(48, 48)).toImage()
 
             if req in reqs.installed:
-                self.assertEqual(icon, QImage(installed))
+                self.assertEqual(icon, QImage(installed),
+                                 f'{req} has incorrect icon: must have `installed` icon!')
             else:
-                self.assertEqual(icon, QImage(not_installed))
+                self.assertEqual(icon, QImage(not_installed),
+                                 f'{req} has incorrect icon: must have `not installed` icon!')
