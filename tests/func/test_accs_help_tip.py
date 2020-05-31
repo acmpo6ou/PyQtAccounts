efac63359eee72bc58dadc077d68401c9caf0c50
Acmpo6ou <acmpo6ou@gmail.com>
2020-05-31 11:32:43 +0300

    Write documentation for test_accs_help_tip.py

--- tests/func/test_accs_help_tip.py
+++ tests/func/test_accs_help_tip.py
@@ -30,18 +30,33 @@ from PyQtAccounts import *
 
 
 class HelpTipTest(AccsTest):
+    """
+    This class provides all functional tests for accounts HelpTip.
+    """
     def test_no_accs(self):
+        """
+        This test tests HelpTip when there is no accounts yet.
+        """
         # Ross opens his database, he has no accounts yet
         super().setUp('a', 'a')
 
         # There is help tip saying how he can create new account
         tip = self.win.accs.tips['help']
-        self.assertEqual(tip.text(), HELP_TIP_ACCS)
+        self.assertEqual(
+            tip.text(), HELP_TIP_ACCS,
+            'Accounts help tip message when there are no'
+            'accounts is incorrect!')
 
     def test_has_accs(self):
+        """
+        This test tests HelpTip when there are accounts.
+        """
         # Emily opens up PyQtAccounts, she has some databases
         super().setUp()
 
         # There is help tip saying that she need to chose account
         tip = self.accs.tips['help']
-        self.assertEqual(tip.text(), "Виберіть акаунт")
+        self.assertEqual(
+            tip.text(), "Виберіть акаунт",
+            'Accounts help tip message when there are '
+            'accounts is incorrect!')
