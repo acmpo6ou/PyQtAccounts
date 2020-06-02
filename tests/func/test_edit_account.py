75f394750351d96b0f1a0cac5c0c1be86bd5e9ef
Acmpo6ou <acmpo6ou@gmail.com>
2020-06-02 16:20:02 +0300

    Write documentation for test_edit_account.py

--- tests/func/test_edit_account.py
+++ tests/func/test_edit_account.py
@@ -28,7 +28,14 @@ from PyQtAccounts import *
 
 
 class EditAccsTest(AccsTest):
+    """
+    This test class provides all functional tests for edit account form.
+    """
     def setUp(self):
+        """
+        Here we reassign some widely used variables and initialize in-memory
+        file system.
+        """
         super().setUp()
         self.form = self.accs.forms['edit']
         self.list = self.accs.list
@@ -46,10 +53,16 @@ class EditAccsTest(AccsTest):
         self.date = self.form.dateInput
         self.comment = self.form.commentInput
 
+        # here we initialize in-memory file system because our tests will change
+        # databases and sometimes save those changes but we don't want to change
+        # our test databases so we use fake file system
         init_src_folder(self.monkeypatch)
         self.copyDatabase('database')
 
     def test_edit_content(self):
+        """
+        Here we test editing of account in edit account form.
+        """
         # Lea wants to edit her account, so she chose it in the list and presses edit button
         self.list.selected(Index('habr'))
         self.editButton.click()
@@ -58,36 +71,60 @@ class EditAccsTest(AccsTest):
         self.checkOnlyVisible(self.form)
 
         # Every field is filled as well
-        self.assertEqual(self.account.text(), 'habr')
-        self.assertEqual(self.name.text(), 'Lea')
-        self.assertEqual(self.pass_input.text(), 'habr_password')
-        self.assertEqual(self.pass_repeat_input.text(), 'habr_password')
-        self.assertEqual(self.email.text(), 'spheromancer@habr.com')
-        self.assertEqual(self.date.text(), '19.05.1990')
-        self.assertEqual(self.comment.toPlainText(), 'Habr account.')
+        self.assertEqual(
+            self.account.text(), 'habr',
+            'account name field of edit form is filled with incorrect data!')
+        self.assertEqual(
+            self.name.text(), 'Lea',
+            'name field of edit form is filled with incorrect data!')
+        self.assertEqual(
+            self.pass_input.text(), 'habr_password',
+            'password field of edit form is filled with incorrect data!')
+        self.assertEqual(
+            self.pass_repeat_input.text(), 'habr_password',
+            'second password field of edit form is filled with incorrect data!'
+        )
+        self.assertEqual(
+            self.email.text(), 'spheromancer@habr.com',
+            'email field of edit form is filled with incorrect data!')
+        self.assertEqual(
+            self.date.text(), '19.05.1990',
+            'date field of edit form is filled with incorrect data!')
+        self.assertEqual(
+            self.comment.toPlainText(), 'Habr account.',
+            'comment field of edit form is filled with incorrect data!')
 
     def test_delete_button(self):
-        # Toon wants to delete his account, so he chose it in the list and presses edit
+        """
+        This test tests delete button of edit account form.
+        """
+        # Toon wants to delete his account, so he chose it in the list and
+        # presses edit button
         self.list.selected(Index('mega'))
         self.editButton.click()
 
         # He then presses delete button
         # Warning message appears and Toon changes his mind pressing `No`
-        self.monkeypatch.setattr(QMessageBox, 'warning',
-                                 self.mess('Увага!',
-                                           'Ви певні що хочете видалити акаунт <i><b>mega</b></i>',
-                                           QMessageBox.No))
+        self.monkeypatch.setattr(
+            QMessageBox, 'warning',
+            self.mess('Увага!',
+                      'Ви певні що хочете видалити акаунт <i><b>mega</b></i>',
+                      QMessageBox.No))
         self.deleteButton.click()
 
         # account `mega` is still in the list and in the database too
         self.checkAccInList('mega')
-        self.assertIn('mega', getAkiList(self.win.db))
+        self.assertIn(
+            'mega', getAkiList(self.win.db),
+            'Account is not in the list when user presses delete '
+            'button and then presses `No` in confirmation dialog!')
 
         # Toon changes his mind again presses delete button and `Yes` in the warning dialog
-        self.monkeypatch.setattr(QMessageBox, 'warning',
-                                 self.mess('Увага!',
-                                           'Ви певні що хочете видалити акаунт <i><b>mega</b></i>',
-                                           QMessageBox.Yes))
+        self.monkeypatch.setattr(
+            QMessageBox, 'warning',
+            self.mess('Увага!',
+                      'Ви певні що хочете видалити акаунт <i><b>mega</b></i>',
+                      QMessageBox.Yes))
         self.deleteButton.click()
 
         # edit form disappears
@@ -97,9 +134,15 @@ class EditAccsTest(AccsTest):
         self.checkAccNotInList('mega')
 
         # And it deleted from database too
-        self.assertNotIn('mega', getAkiList(self.win.db))
+        self.assertNotIn(
+            'mega', getAkiList(self.win.db),
+            'Account has not deleted from database after user'
+            'presses delete button in edit account form!')
 
     def test_save_button(self):
+        """
+        Here we test save button of edit account form.
+        """
         # Tom wants to edit database
         self.list.selected(Index('mega'))
         self.editButton.click()
@@ -121,20 +164,31 @@ class EditAccsTest(AccsTest):
 
         # Account name changes in the list and in database
         self.checkAccInList('stackoverflow')
-        self.assertIn('stackoverflow', getAkiList(self.win.db))
+        self.assertIn(
+            'stackoverflow', getAkiList(self.win.db),
+            'Account name has not changed in account list after '
+            'editing it from edit account form!')
 
         # Also it has all properties as well
         account = self.win.db['stackoverflow']
-        self.assertEqual('stackoverflow', account.account)
-        self.assertEqual(b'mypass', account.password)
-        self.assertEqual('Tom', account.name)
-        self.assertEqual('tom@gmail.com', account.email)
-        self.assertEqual('10.07.1997', account.date)
-        self.assertEqual('I love stackoverflow!', account.comment)
+        self.assertEqual('stackoverflow', account.account,
+                         'account name of edited account is incorrect!')
+        self.assertEqual(b'mypass', account.password,
+                         'password of edited account is incorrect!')
+        self.assertEqual('Tom', account.name,
+                         'name of edited account is incorrect!')
+        self.assertEqual('tom@gmail.com', account.email,
+                         'email of edited account is incorrect!')
+        self.assertEqual('10.07.1997', account.date,
+                         'date of edited account is incorrect!')
+        self.assertEqual('I love stackoverflow!', account.comment,
+                         'comment of edited account is incorrect!')
 
         # And there is no longer `mega` in the list neither in database
         self.checkAccNotInList('mega')
-        self.assertNotIn('mega', getAkiList(self.win.db))
+        self.assertNotIn(
+            'mega', getAkiList(self.win.db),
+            'Old replica of edited account is still in the database!')
 
         # Tom then wants to view edited account
         self.list.selected(Index('stackoverflow'))
