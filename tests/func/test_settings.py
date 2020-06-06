8c27d7b94a83b87e2386c04c1d7bee815fe86dc3
Acmpo6ou <acmpo6ou@gmail.com>
2020-06-06 15:43:43 +0300

    Start writing documentation for test_settings.py

--- tests/func/test_settings.py
+++ tests/func/test_settings.py
@@ -28,18 +28,38 @@ from PyQtAccounts import *
 
 
 class SettingsTest(SettingsMixin, DbsTest):
+    """
+    This test class provides all functional tests for PyQtAccounts settings.
+    """
     def setUp(self):
+        """
+        Here we setup DbsTest because this is a functional test about main
+        window and also we setup SettingsMixin because this test is about
+        settings, and SettingsMixin initializes in-memory file system.
+        """
         DbsTest.setUp(self)
         SettingsMixin.setUp(self)
 
     def test_settings_show_menu(self):
+        """
+        Here we test whether settings show properly when we access them through
+        menu.
+        """
+        # here we can't use self.menu() as it operates on self.window attribute
+        # which we don't define
         window = Window()
         edit = window.menuBar().actions()[1]  # second is `Edit` submenu
         pref = edit.menu().actions()[0]
         pref.trigger()
-        self.assertTrue(window.settings.visibility)
+        self.assertTrue(
+            window.settings.visibility,
+            "Settings aren't showed when user goes to "
+            "menu: Edit -> Preferences!")
 
     def test_is_main_db_True(self):
+        """
+        Here we test main database feature of PyQtAccounts.
+        """
         # Bob uses main database feature of PyQtAccounts, he has it turned on
         # and `main` is the main database.
         self.settings.setValue('advanced/is_main_db', True)
@@ -48,15 +68,29 @@ class SettingsTest(SettingsMixin, DbsTest):
         # He opens PyQtAccounts and see form for opening his `main` database.
         # And the title of this form says `Відкрити базу данних main`
         window = Window()
-        self.assertIn('main', window.dbs.forms['open'].title.text())
+        self.assertIn(
+            'main', window.dbs.forms['open'].title.text(),
+            "The title of open database form is incorrect when user has main database"
+            "feature turned on and he launches PyQtAccounts, it doesn't "
+            "contain name of main database!")
 
         # Checkbox in settings is also checked
-        self.assertTrue(window.settings.mainDbLayout.checkbox.isChecked())
+        self.assertTrue(
+            window.settings.mainDbLayout.checkbox.isChecked(),
+            "The checkbox in settings that represents main database feature"
+            "isn't checked when user has this feature turned on!")
 
         # And combobox have `main` as current database
-        self.assertEqual('main', window.settings.mainDbLayout.dbs.currentText())
+        self.assertEqual(
+            'main',
+            window.settings.mainDbLayout.dbs.currentText()
+            "Main database in combobox is incorrect - must be `main`!")
 
     def test_is_main_db_not_set(self):
+        """
+        Here we test main database feature of PyQtAccounts when it isn't set
+        (i.e. we test its default settings). 
+        """
         # Tom doesn't know about main database feature of PyQtAccounts yet.
         # He has it turned off by default.
         self.settings.remove('advanced/is_main_db')
@@ -65,13 +99,19 @@ class SettingsTest(SettingsMixin, DbsTest):
         # He opens PyQtAccounts and there is no form for opening any database.
         # Title of open database form is empty.
         window = Window()
-        self.assertEqual('<b></b>', window.dbs.forms['open'].title.text())
+        self.assertEqual(
+            '<b></b>', window.dbs.forms['open'].title.text(),
+            "The title of open database form is incorrect when user has main database"
+            "feature unset (i.e. by default turned off), title contains name "
+            "of main database!")
 
         # Checkbox in settings is also unchecked
-        self.assertFalse(window.settings.mainDbLayout.checkbox.isChecked())
+        self.assertFalse(window.settings.mainDbLayout.checkbox.isChecked(),
+                         "The checkbox in settings that represents main database feature")
 
         # And combobox have `main` as current database
-        self.assertEqual('main', window.settings.mainDbLayout.dbs.currentText())
+        self.assertEqual('main',
+                         window.settings.mainDbLayout.dbs.currentText())
 
     def test_is_main_db_False(self):
         # Ross doesn't use main database feature of PyQtAccounts.
@@ -88,4 +128,5 @@ class SettingsTest(SettingsMixin, DbsTest):
         self.assertFalse(window.settings.mainDbLayout.checkbox.isChecked())
 
         # And combobox have `crypt` as current database
-        self.assertEqual('crypt', window.settings.mainDbLayout.dbs.currentText())
+        self.assertEqual('crypt',
+                         window.settings.mainDbLayout.dbs.currentText())
