b6ec70714aa519687ead45ecbf0f4a863996be7e
Acmpo6ou <acmpo6ou@gmail.com>
2020-06-15 12:52:38 +0300

    Finish test_account_serialization and test_dumps tests, start writing test for loads function and how it loads database that is serialized in obsolete way.

--- tests/unit/test_akidump.py
+++ tests/unit/test_akidump.py
@@ -17,6 +17,7 @@
 # along with PyQtAccounts.  If not, see <https://www.gnu.org/licenses/>.
 
 from core.akidump import *
+from core.getaki import *
 
 from unittest.mock import Mock
 import pytest
@@ -43,10 +44,16 @@ class AkidumpTest(UnitTest):
 
     def test_account_serialization(self):
         """
-        Here we test how json will serialize our Account class.
+        Here we test how json will serialize our Account class using our
+        `to_dict` method that converts our Account instance to dictionary.
         """
         dump = json.dumps(self.db['gmail'].to_dict())
-        self.assertEqual(dump, '{}')
+        self.assertEqual(
+            dump,
+            '{"account": "gmail", "name": "Tom", "email": "tom@gmail.com", '
+            '"password": "123", "date": "01.01.1990", '
+            '"comment": "My gmail account.", "copy_email": true}',
+            '`to_dict` method of Account class is incorrect!')
 
     def test_dumps(self):
         """
@@ -56,10 +63,50 @@ class AkidumpTest(UnitTest):
         # here we use dumps function for akidump.py module to serialize his
         # account
         dump = dumps(self.db)
+        print(repr(dump))
 
         # then we check that it is dumped appropriately
         self.assertEqual(
-            dump, '{"gmail": {"account": "gmail", "name": "Tom", "email": '
-            '"tom@gmail.com", "password": "123", "date": "01.01.1990"}}',
+            dump, b'{"gmail": {"account": "gmail", "name": "Tom", "email": '
+            b'"tom@gmail.com", "password": "123", "date": "01.01.1990", '
+            b'"comment": "My gmail account.", "copy_email": true}}',
             "Serialization of `dumps` function from core.akidump module is incorrect!"
         )
+
+    def test_loads_obsolete(self):
+        """
+        Here we test how `loads` function from akidump.py module will
+        deserialize data that is serialized in obsolete way.
+        """
+        # we will use openDatabase from core.getaki module which uses loads
+        # function from akidump to deserialize database
+        db = openDatabase(
+            'database',  # `database` serialized in obsolete way
+            b'some_password')
+
+        # here we check whether it deserialized appropriately
+        # we expect next 3 accounts in deserialized database
+        habr = Account(account='habr',
+                       name='Lea',
+                       email='spheromancer@habr.com',
+                       password='habr_password',
+                       date='19.05.1990',
+                       comment='Habr account.',
+                       copy_email=True)
+        gmail = Account(account='gmail',
+                        name='Bob',
+                        email='bobgreen@gmail.com',
+                        password='$z#5G_UG~K;I9U9$',
+                        date='19.05.1990',
+                        comment='Gmail account.',
+                        copy_email=True)
+        mega = Account(account='mega',
+                       name='Tom',
+                       email='tom@gmail.com',
+                       password='tom',
+                       date='01.01.2000',
+                       comment='Mega account.',
+                       copy_email=True)
+        expected_db = {'habr': habr, 'gmail': gmail, 'mega': mega}
+        self.assertEqual(db, expected_db)
+        # TODO: use isEqual function form utils.py to compare databases
