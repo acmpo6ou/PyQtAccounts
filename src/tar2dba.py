b54be5bcb0e4e69422879b16845b57c8b3b051fe
acmpo6ou <acmpo6ou@gmail.com>
2021-10-24 09:02:34 +0300

    Implement tar2dba – a script to convert database files to .dba

--- src/tar2dba.py
+++ src/tar2dba.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 
+import glob
+
+for db in glob.glob("*.db"):
+    name = db.rstrip(".db")
+    salt = open(f"{name}.bin", "rb").read()
+    db = open(db, "rb").read()
+
+    with open(f"{name}.dba", "wb") as dba:
+        dba.write(salt)
+        dba.write(db)
+
+
